cb6dfb3b7240448e4d97b79c587b3f9bfb50e86f
gototkno1 <gototkno1@gmail.com>
2026-02-11 23:12:17 +0900

    fix: Fix imports in ContentRepository

--- src/main/java/app/project/platform/repository/ContentRepository.java
+++ src/main/java/app/project/platform/repository/ContentRepository.java
@@ -1,20 +1,14 @@
 package app.project.platform.repository;
 
 import app.project.platform.entity.Content;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 
-import javax.swing.text.html.Option;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
